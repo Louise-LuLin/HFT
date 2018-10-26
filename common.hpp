a3a4b32d352c92b70d575bc0b6d40d9f9089a5b4
Lu Lin <lulin199209@gmail.com>
2018-10-26 15:55:12 -0400

    minor

--- common.hpp
+++ common.hpp
@@ -253,7 +253,7 @@ public:
       
       if(userIds.find(uName) == userIds.end()){
         printf("[err]User %s not exist\n", uName.c_str());
-        assert(userIds.find(uName) < userIds.end());
+        assert(!userIds.find(uName) == userIds.end());
       }
       else
         user_idx = userIds[uName];
@@ -261,7 +261,7 @@ public:
       std::string voteKey = std::to_string(user_idx) + "_" + std::to_string(cur_Id);
       if(rV.find(voteKey) == rV.end()){
         printf("[err]User-id pair %s not exist\n", voteKey.c_str());
-        assert(rV.find(voteKey) < rV.end());
+        assert(!rV.find(voteKey) == rV.end());
       }
       else
         CVIndex[rV[voteKey]] = mask;
